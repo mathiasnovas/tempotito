06365b7a538525bed664e6be517e9730a7745eff
Mathias Novas <mathias_novas@hotmail.com>
2020-11-01 22:04:13 +0100

    Update responsiveness

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "tap-bpm.git"
-set :repo_url, "git@github.com:mathiasnovas/tap-bpm.git"
+set :application, "tempotito.git"
+set :repo_url, "git@github.com:mathiasnovas/tempotito.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/srv/users/serverpilot/apps/tempotito"
